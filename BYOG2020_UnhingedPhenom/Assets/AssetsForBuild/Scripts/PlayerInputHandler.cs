cf6f4e5b6f8f5a6547c5cf90ce67a6a01b908310
ayan <>
2020-10-31 09:27:46 +0530

    Basic controller setup

---
+++ BYOG2020_UnhingedPhenom/Assets/AssetsForBuild/Scripts/PlayerInputHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInputHandler : MonoBehaviour
+{
+    public Vector2 InputVector { get; private set; }
+
+    public Vector3 MousePosition { get; private set; }
+
+    private float _horizontal;
+    private float _vertical;
+
+    private void Update()
+    {
+        _horizontal = Input.GetAxis("Horizontal");
+        _vertical = Input.GetAxis("Vertical");
+
+        InputVector = new Vector2(_horizontal, _vertical);
+
+        MousePosition = Input.mousePosition;
+    }
+}
---
+++ BYOG2020_UnhingedPhenom/Assets/AssetsForBuild/Scripts/PlayerMovementHandler.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerInputHandler))]
+public class PlayerMovementHandler : MonoBehaviour
+{
+    [SerializeField] private Camera _camera;
+
+    [SerializeField] private float m_MovementSpeed;
+    [SerializeField] private float m_RotationSpeed;
+
+    [SerializeField] private bool _rotateTowardsMouse;
+    
+    private PlayerInputHandler _playerInput;
+
+
+    private void Awake()
+    {
+        _playerInput = GetComponent<PlayerInputHandler>();
+    }
+
+    private void Update()
+    {
+        var targetVector = new Vector3(_playerInput.InputVector.x, 0, _playerInput.InputVector.y);
+        var movementVector = MoveTowardTarget(targetVector);
+
+        if (!_rotateTowardsMouse)
+        {
+            RotateTowardMovementVector(movementVector);
+        }
+        if (_rotateTowardsMouse)
+        {
+            RotateFromMouseVector();
+        }
+    }
+
+    private void RotateFromMouseVector()
+    {
+        Ray ray = _camera.ScreenPointToRay(_playerInput.MousePosition);
+
+        if (Physics.Raycast(ray, out RaycastHit hitInfo, maxDistance: 300f))
+        {
+            var target = hitInfo.point;
+            target.y = transform.position.y;
+            transform.LookAt(target);
+        }
+    }
+
+    private void RotateTowardMovementVector(Vector3 movementDirection)
+    {
+        if (movementDirection.magnitude == 0) { return; }
+        var rotation = Quaternion.LookRotation(movementDirection);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, m_RotationSpeed);
+    }
+
+    private Vector3 MoveTowardTarget(Vector3 targetVector)
+    {
+        var speed = m_MovementSpeed * Time.deltaTime;
+
+        targetVector = Quaternion.Euler(0, _camera.gameObject.transform.rotation.eulerAngles.y, 0) * targetVector;
+        var targetPosition = transform.position + targetVector * speed;
+        transform.position = targetPosition;
+        return targetVector;
+    }
+}
